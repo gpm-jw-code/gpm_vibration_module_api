09b097e926f3d84a0cc742f444aac395832be3d4
jinweihuang <jinweihuang@gpmcorp.com.tw>
2020-10-05 18:43:41 +0800

    變更版號 1.10.5.0 @@@從此版開始版號為 主版本(1、2...).月.日.流水

--- gpm_vibration_module_api/Properties/AssemblyInfo.cs
+++ gpm_vibration_module_api/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("gpm_vibration_module_api")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("gpm_vibration_module_api[YCM]")]
+[assembly: AssemblyDescription("Customized For YCM")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("GPM")]
 [assembly: AssemblyProduct("gpm_vibration_module_api")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以使用 '*' 將組建和修訂編號
 // 設為預設，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.10.5.0")]
+[assembly: AssemblyFileVersion("1.10.5.0")]
